ccfec2d0778e002e667f8007db2e439e03e82918
sing3demons <sing3demons@live.com>
2026-04-05 14:06:05 +0700

    feat: Refactor logging setup by moving NewLogger to config package and simplifying main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,109 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
-	"log/slog"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 
 	"github.com/sing3demons/oauth_server/internal/adapters/mongo_store"
 	"github.com/sing3demons/oauth_server/internal/adapters/redis_store"
 	"github.com/sing3demons/oauth_server/internal/config"
 	"github.com/sing3demons/oauth_server/internal/core/services"
 	"github.com/sing3demons/oauth_server/internal/handlers"
-	"github.com/sing3demons/oauth_server/pkg/logger"
 	"github.com/sing3demons/oauth_server/pkg/middleware"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
-
-func buildLogFilename(props config.LogFileProperties) string {
-	datePattern := props.DatePattern
-	datePattern = strings.ReplaceAll(datePattern, "YYYY", "2006")
-	datePattern = strings.ReplaceAll(datePattern, "MM", "01")
-	datePattern = strings.ReplaceAll(datePattern, "DD", "02")
-	datePattern = strings.ReplaceAll(datePattern, "HH", "15")
-
-	dateStr := time.Now().Format(datePattern)
-
-	filename := props.Filename
-	if strings.Contains(filename, "%DATE%") {
-		filename = strings.ReplaceAll(filename, "%DATE%", dateStr)
-	}
-
-	return fmt.Sprintf("%s/%s%s", props.Dirname, filename, props.Extension)
-}
-
-func NewLogger(cfg *config.Config) (*logger.SlogAdapter, *logger.SlogAdapter, *logger.DefaultMaskingService) {
-	var maskingSvc = &logger.DefaultMaskingService{}
-
-	// 1. Setup Detail Logger
-	detailWriter := io.MultiWriter(os.Stdout)
-	if cfg.LoggerConfig.Detail.EnableFileLogging {
-		os.MkdirAll(cfg.LoggerConfig.Detail.LogFileProperties.Dirname, 0755)
-		path := buildLogFilename(cfg.LoggerConfig.Detail.LogFileProperties)
-
-		detailRotateLogger := &lumberjack.Logger{
-			Filename:   path,
-			MaxSize:    cfg.LoggerConfig.Detail.Rotation.MaxSize / (1024 * 1024), // converting bytes to megabytes
-			MaxBackups: cfg.LoggerConfig.Detail.Rotation.MaxBackups,
-			MaxAge:     cfg.LoggerConfig.Detail.Rotation.MaxAge,
-			Compress:   cfg.LoggerConfig.Detail.Rotation.Compress,
-		}
-		writers := []io.Writer{detailRotateLogger}
-		if cfg.LoggerConfig.Detail.Console {
-			writers = append(writers, os.Stdout)
-		}
-		detailWriter = io.MultiWriter(writers...)
-	}
-
-	// 2. Setup Summary Logger
-	summaryWriter := io.MultiWriter(os.Stdout)
-	if cfg.LoggerConfig.Summary.EnableFileLogging {
-		os.MkdirAll(cfg.LoggerConfig.Summary.LogFileProperties.Dirname, 0755)
-		path := buildLogFilename(cfg.LoggerConfig.Summary.LogFileProperties)
-
-		summaryRotateLogger := &lumberjack.Logger{
-			Filename:   path,
-			MaxSize:    cfg.LoggerConfig.Summary.Rotation.MaxSize / (1024 * 1024), // converting bytes to megabytes
-			MaxBackups: cfg.LoggerConfig.Summary.Rotation.MaxBackups,
-			MaxAge:     cfg.LoggerConfig.Summary.Rotation.MaxAge,
-			Compress:   cfg.LoggerConfig.Summary.Rotation.Compress,
-		}
-		writers := []io.Writer{summaryRotateLogger}
-		if cfg.LoggerConfig.Summary.Console {
-			writers = append(writers, os.Stdout)
-		}
-		summaryWriter = io.MultiWriter(writers...)
-	}
-
-	detailHandler := slog.NewJSONHandler(detailWriter, &slog.HandlerOptions{
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if a.Key == slog.MessageKey {
-				return slog.Attr{}
-			}
-			return a
-		},
-	})
-	detailSlogAdapter := logger.NewSlogAdapter(slog.New(detailHandler))
-
-	summaryHandler := slog.NewJSONHandler(summaryWriter, &slog.HandlerOptions{
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if a.Key == slog.MessageKey {
-				return slog.Attr{}
-			}
-			return a
-		},
-	})
-	summarySlogAdapter := logger.NewSlogAdapter(slog.New(summaryHandler))
-	return detailSlogAdapter, summarySlogAdapter, maskingSvc
-}
 
 func main() {
 	cfg := config.LoadConfig()
-	detailSlogAdapter, summarySlogAdapter, maskingSvc := NewLogger(cfg)
+	detailSlogAdapter, summarySlogAdapter, maskingSvc := config.NewLogger(cfg)
 
 	// Init MongoDB
 	mongoClient, err := mongo_store.NewMongoClient(cfg.MongoURI)
